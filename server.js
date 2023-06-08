40e49915e2394e255d15bdd10d28e9be93995006
capahdan <2081029@unai.edu>
2023-06-08 21:02:31 +0700

    Add Swagger

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
 
 const app = express();
 
@@ -18,28 +20,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./src/models");
 db.sequelize.sync()
-  .then(() => {
-    console.log("Synced db.");
-  })
-  .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
-  });
+.then(() => {
+  console.log("Synced db.");
+})
+.catch((err) => {
+  console.log("Failed to sync db: " + err.message);
+});
 
 // // drop the table if it already exists
 // db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-
-// routes
-require('./src/routes/auth.routes')(app);
-require('./src/routes/user.routes')(app);
-require("./src/routes/articles.routes")(app);
-require("./src/routes/questions.routes")(app);
-require("./src/routes/answers.routes")(app);
-
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+  //   console.log("Drop and re-sync db.");
+  // });
+  
+  // routes
+  require('./src/routes/auth.routes')(app);
+  require('./src/routes/user.routes')(app);
+  require("./src/routes/articles.routes")(app);
+  require("./src/routes/questions.routes")(app);
+  require("./src/routes/answers.routes")(app);
+  
+  
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+  // set port, listen for requests
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
